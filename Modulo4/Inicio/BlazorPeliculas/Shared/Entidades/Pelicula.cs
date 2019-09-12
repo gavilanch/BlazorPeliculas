c74ed98bbc38a8cee4619fc07e253fa4fc5c6011
Felipe Gavilan <elmejor_887@hotmail.com>
2019-09-12 13:56:02 -0400

    modulo 4

---
+++ Modulo4/Inicio/BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+    }
+}
