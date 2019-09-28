0aac6b56f9d4d612795206deead024f8b87c3e47
Felipe Gavilan <elmejor_887@hotmail.com>
2019-09-28 11:05:14 -0400

    modulo 7

---
+++ Modulo8/Hosted/BlazorPeliculas/Server/Migrations/20190923152345_VotosPeliculas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorPeliculas.Server.Migrations
+{
+    public partial class VotosPeliculas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VotosPeliculas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Voto = table.Column<int>(nullable: false),
+                    FechaVoto = table.Column<DateTime>(nullable: false),
+                    PeliculaId = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VotosPeliculas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VotosPeliculas_Peliculas_PeliculaId",
+                        column: x => x.PeliculaId,
+                        principalTable: "Peliculas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VotosPeliculas_PeliculaId",
+                table: "VotosPeliculas",
+                column: "PeliculaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VotosPeliculas");
+        }
+    }
+}
---
+++ Modulo8/Hosted/BlazorPeliculas/Shared/DTOs/RolDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPeliculas.Shared.DTOs
+{
+    public class RolDTO
+    {
+        public string RoleId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
