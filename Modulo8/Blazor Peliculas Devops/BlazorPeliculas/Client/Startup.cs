427307e461713e49bb9aa4cca947228cb11f8cbf
Felipe Gavilan <elmejor_887@hotmail.com>
2019-09-30 17:08:09 -0400

    added azure devops example

---
+++ Modulo8/Blazor Peliculas Devops/BlazorPeliculas/Client/Startup.cs
@@ -0,0 +1,33 @@
+using BlazorPeliculas.Client.Auth;
+using BlazorPeliculas.Client.Helpers;
+using BlazorPeliculas.Client.Repositorios;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorPeliculas.Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ServicioSingleton>();
+            services.AddTransient<ServicioTransient>();
+            services.AddScoped<IRepositorio, Repositorio>();
+            services.AddScoped<IMostrarMensajes, MostrarMensajes>();
+            services.AddAuthorizationCore();
+
+            services.AddScoped<ProveedorAutenticacionJWT>();
+
+            services.AddScoped<AuthenticationStateProvider, ProveedorAutenticacionJWT>(
+                provider => provider.GetRequiredService<ProveedorAutenticacionJWT>());
+            services.AddScoped<ILoginService, ProveedorAutenticacionJWT>(
+                provider => provider.GetRequiredService<ProveedorAutenticacionJWT>());
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
