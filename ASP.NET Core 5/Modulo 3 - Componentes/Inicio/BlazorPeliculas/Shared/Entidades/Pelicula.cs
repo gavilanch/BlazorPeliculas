1654686b189b8dcb0a634bc5a7fb6e12534e50a8
Felipe Gavilan <elmejor_887@hotmail.com>
2020-11-24 13:58:48 -0400

    Agregado modulo 3

---
+++ ASP.NET Core 5/Modulo 3 - Componentes/Inicio/BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+    }
+}
