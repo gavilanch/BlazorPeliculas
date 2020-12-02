c1b5318320d699c5e37f052d56a628d56b09f2a9
Felipe Gavilan <elmejor_887@hotmail.com>
2020-12-02 14:35:32 -0400

    Agregado modulo 4

---
+++ ASP.NET Core 5/Modulo 4 - Ruteo/Inicio/BlazorPeliculas/Client/Pages/Counter.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static BlazorPeliculas.Client.Shared.MainLayout;
+
+namespace BlazorPeliculas.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] ServicioSingleton singleton { get; set; }
+        [Inject] ServicioTransient transient { get; set; }
+        [Inject] protected IJSRuntime JS { get; set; }
+        [CascadingParameter] protected AppState appState { get; set; }
+
+        IJSObjectReference modulo;
+
+        protected int currentCount = 0;
+        static int currentCountStatic = 0;
+
+        [JSInvokable]
+        public async Task IncrementCount()
+        {
+
+            modulo = await JS.InvokeAsync<IJSObjectReference>("import", "./js/Counter.js");
+            await modulo.InvokeVoidAsync("mostrarAlerta", "hola mundo");
+
+            currentCount++;
+            singleton.Valor = currentCount;
+            transient.Valor = currentCount;
+            currentCountStatic++;
+            await JS.InvokeVoidAsync("pruebaPuntoNetStatic");
+        }
+
+        protected async Task IncrementCountJavacript()
+        {
+            await JS.InvokeVoidAsync("pruebaPuntoNETInstancia",
+                    DotNetObjectReference.Create(this));
+        }
+
+        [JSInvokable]
+        public static Task<int> ObtenerCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
+        }
+    }
+}
