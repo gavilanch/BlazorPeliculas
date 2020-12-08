e2d9c1ef849e9567a169ba03eb22a16faffe16d4
Felipe Gavilan <elmejor_887@hotmail.com>
2020-12-08 16:38:05 -0400

    Agregado modulo 6

---
+++ ASP.NET Core 5/Modulo 6 - HTTP y Entity Framework Core/Fin/BlazorPeliculas/Server/Helpers/IAlmacenadorArchivos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Server.Helpers
+{
+    public interface IAlmacenadorArchivos
+    {
+        Task<string> GuardarArchivo(byte[] contenido, string extension, string nombreContenedor);
+        Task EliminarArchivo(string ruta, string nombreContenedor);
+        Task<string> EditarArchivo(byte[] contenido, string extension, string nombreContenedor, string ruta);
+    }
+}
---
+++ ASP.NET Core 5/Modulo 6 - HTTP y Entity Framework Core/Fin/BlazorPeliculas/Server/Startup.cs
@@ -0,0 +1,67 @@
+using AutoMapper;
+using BlazorPeliculas.Server.Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace BlazorPeliculas.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IAlmacenadorArchivos, AlmacenadorArchivosAzStorage>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddControllersWithViews().AddNewtonsoftJson(
+                options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
