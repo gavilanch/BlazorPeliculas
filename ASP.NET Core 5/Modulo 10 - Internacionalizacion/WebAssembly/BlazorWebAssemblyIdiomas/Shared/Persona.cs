4b8584803cdca724a3519c6423c6e1bde985a0d7
Felipe Gavilan <elmejor_887@hotmail.com>
2020-12-23 09:40:48 -0400

    Agregado modulo 10

---
+++ ASP.NET Core 5/Modulo 10 - Internacionalizacion/WebAssembly/BlazorWebAssemblyIdiomas/Shared/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorWebAssemblyIdiomas.Shared
+{
+    public class Persona
+    {
+        [Required(
+            ErrorMessageResourceType = typeof(Recursos.Resource), 
+            ErrorMessageResourceName = nameof(Recursos.Resource.required))]
+        public string Nombre { get; set; }
+    }
+}
