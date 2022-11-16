178163030c5966556cc7d24dcd2c64fe9578b28b
Felipe Gavilan <elmejor_887@hotmail.com>
2022-11-16 17:57:51 -0400

    Agregado nuevo modulo

---
+++ ASP.NET Core 7/Modulo 2 - Introduccion a Razor/Fin/BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; } = null!;
+        public DateTime FechaLanzamiento { get; set; }
+    }
+}
