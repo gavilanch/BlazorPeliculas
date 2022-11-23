02e6c90be36eddc2f8636df33ba618f2840638f9
Felipe Gavilan <elmejor_887@hotmail.com>
2022-11-23 11:34:15 -0400

    Agregado nuevo modulo

---
+++ ASP.NET Core 7/Modulo 4 - Ruteo/Inicio/BlazorPeliculas/Client/wwwroot/js/Utilidades.js
@@ -0,0 +1,10 @@
+﻿function pruebaPuntoNetStatic() {
+    DotNet.invokeMethodAsync("BlazorPeliculas.Client", "ObtenerCurrentCount")
+        .then(resultado => {
+            console.log('conteo desde javascript ' + resultado);
+        })
+}
+
+function pruebaPuntoNetInstancia(dotnetHelper) {
+    dotnetHelper.invokeMethodAsync("IncrementCount");
+}
